5647fa5c65d4d60fbccc04230439634b0fc11f8c
helsinki2103 <minhngoc02hn@gmail.com>
2021-04-29 09:57:18 +0700

    Updated

--- Test.java
+++ Test.java
@@ -9,6 +9,6 @@ class Test{
 		return res + res;
 	}
 	public static void main(String[] args) {
-		for(int i = 1; i <= 100; i++) System.out.println(i);
+		for(int i = 1; i <= 10; i++) System.out.println(i);
 	}
 }
--- test.cpp
+++ test.cpp
@@ -18,10 +18,7 @@ bool cmp(pair<int, char> a, pair<int, char> b) {
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int n; cin >> n;
-    for(int i = 1; i <= n; i++) {
-        cout << i << " ";
-    }
-    cout << "Hello World\n";
-    return 1;
+    cout << "Hello, World!\n";
+
+    return 0;
 }
