ba85c4d03805b377e7aceadde6d7c5a5044655dd
helsinki2103 <minhngoc02hn@gmail.com>
2021-05-30 09:03:05 +0700

    Update

--- Coding Problems/Codeforces/1529B.py
+++ Coding Problems/Codeforces/1529B.py
@@ -1,23 +1,19 @@
 import sys
-sys.stdin = open('input.txt','r')
+# sys.stdin = open('input.txt','r')
 input = sys.stdin.readline
 
 for _ in range(int(input())):
     n = int(input())
     a = list(map(int,input().split()))
     ans = 0
-    b = []
-    for x in a:
-        if x <= 0:
-            b.append(x)
-    b.sort()
-
-    for x in range(n):
+    a.sort()
+    ans = sum([x <= 0 for x in a])
+    mn = min([x if x > 0 else 10**9 + 7 for x in a])
+    flag = (mn < 10**9 + 7)
+    for i in range(1,n):
         if a[i] <= 0:
-            continue
-        if a[i] - b[-1] > 0:
-            b.append(a[i])
-            break
-    print(len(b))
-
-
+            flag &= (a[i] - a[i - 1] >= mn)
+    if flag == True:
+        print(ans + 1)
+    else:
+        print(ans)
