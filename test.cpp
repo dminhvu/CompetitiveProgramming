4673124ef520f98ed878a93d36fc01e13af34c9f
desmond <minhngoc02hn@gmail.com>
2021-11-27 21:50:35 +0700

    Update

--- test.cpp
+++ test.cpp
@@ -9,8 +9,38 @@ using namespace std;
 template<class t, class u> bool maxi(t &a, u b){ if (a < b){ a = b; return 1;} return 0;}
 template<class t, class u> bool mini(t &a, u b){ if (a > b){ a = b; return 1;} return 0;}
 
+struct Data {
+   char c;
+   int t, k;
+};
+
+int Dijkstra(int** graph, int src, int dst) {
+	// TODO: return the length of shortest path from src to dst.
+	int n = 0;
+	while (&graph[n + 1][0] != NULL) n++;
+   n -= 1;
+	cout << n << '\n';
+	return 1;
+}
+
 int main() {
-   ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-   cout << "Hello\n";
-   return 0;
+   int init[8][6] = {
+	{0, 10, 20, 0, 0, 0},
+	{10, 0, 0, 50, 10, 0},
+	{20, 0, 0, 20, 33, 0},
+	{0, 50, 20, 0, 20, 2},
+	{0, 10, 33, 20, 0, 1},
+	{0, 0, 0, 2, 1, 0},
+   {0, 0, 0, 2, 1, 0},
+   {0, 0, 0, 2, 1, 0}, };
+
+   int n = 8;
+   int** graph = new int*[n];
+   for (int i = 0; i < n; ++i) {
+      graph[i] = init[i];
+      // for (int j = 0; j < 6; j++) cout << graph[i][j] << " ";
+      // cout << '\n';
+   }
+
+   cout << Dijkstra(graph, 0, 0);
 }
