6d81cb632a8486973caa8c4e2e0b35ff3e980966
desmond <minhngoc02hn@gmail.com>
2021-12-25 08:50:50 +0700

    Update

---
+++ Coding Problems/Codeforces/1620E.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < int(n); i++)
+#define fore(i,a,b) for (int i = int(a); i <= int(b); i++)
+#define ford(i,a,b) for (int i = int(a); i >= int(b); i--)
+#define ll int64_t
+#define sz(x) int(x.size())
+
+using namespace std;
+template<class t, class u> bool maxi(t &a, u b){ if (a < b){ a = b; return 1;} return 0;}
+template<class t, class u> bool mini(t &a, u b){ if (a > b){ a = b; return 1;} return 0;}
+const int N = 5e5 + 5;
+
+int main() {
+    // freopen("input.txt","r",stdin);
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int q; cin >> q;
+    int cnt = 0;
+    vector<vector<int> > query(q);
+    int max_element = 0;
+    rep(i,q) {
+        int t; cin >> t;
+        query[i].push_back(t);
+        if (t == 1) {
+            int u; cin >> u;
+            query[i].push_back(u);
+            max_element = max(max_element, u);
+        } else {
+            int u, v; cin >> u >> v;
+            query[i].push_back(u);
+            query[i].push_back(v);
+            max_element = max(max_element, u);
+            max_element = max(max_element, v);
+        }
+    }
+    vector<int> par(N), ans;
+    rep(i,N) par[i] = i;
+    ford(i,q-1,0) {
+        if (query[i][0] == 1) {
+            ans.push_back(par[query[i][1]]);
+        } else {
+            int u = query[i][1], v = query[i][2];
+            par[u] = par[v];
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    for(int x: ans) cout << x << " ";
+    return 0;
+}
